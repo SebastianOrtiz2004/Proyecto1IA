de1fba5e44574c95074c59d51b71e841fa6ffdfe
Sebastián Ortiz <sortiz7841@uta.edu.ec>
2026-04-14 16:52:22 -0500

    cambios

--- src/AlgoritmoGenetico/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-AlgoritmoGenetico/__init__.py
-Inicialización del paquete de optimización evolutiva.
-"""
-# Punto de entrada del paquete AG para importar desde app.py.
-from .optimizador import ejecutar_ag, despacho_voraz
-from .config import GENERADORES, N_GENERADORES
